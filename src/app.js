f0972d01509da19ef42f91b2ff7e81d753be7baa
Willian Oliveira <williangd@yahoo.com.br>
2020-06-11 15:05:34 -0300

    Challenge concluded

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,63 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+function checkRepositoryId(request, response, next) {
+  const { id } = request.params;
+  const index = repositories.findIndex((repo) => repo.id === id);
+  if (index < 0) {
+    return response.status(400).json({ message: 'Repository not found' });
+  }
+
+  request.repositoryIndex = index;
+  next();
+}
+
+app.use('/repositories/:id', checkRepositoryId);
+
+app.get('/repositories', (request, response) => {
+  response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repo = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repo);
+
+  response.json(repo);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { repositoryIndex } = request;
+
+  const { title, url, techs } = request.body;
+
+  repositories[repositoryIndex] = {
+    ...repositories[repositoryIndex],
+    title,
+    url,
+    techs,
+  };
+
+  response.json(repositories[repositoryIndex]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { repositoryIndex } = request;
+
+  repositories.splice(repositoryIndex, 1);
+
+  response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const { repositoryIndex } = request;
+
+  const repo = repositories[repositoryIndex];
+
+  repo.likes++;
+
+  response.json(repo);
 });
 
 module.exports = app;
